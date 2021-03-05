7644563f42e9be4f27f780e9e28359c91e0d831e
isabellenoelnelson <isabellenoelnelson@gmail.com>
2021-03-05 16:15:41 -0500

    LS and PWD working

--- bash.js
+++ bash.js
@@ -1,12 +1,14 @@
 const pwd = require('./pwd');
+const ls = require('./ls');
 
 process.stdout.write('prompt > ');
 process.stdin.on('data', (data) => {
     const cmd = data.toString().trim();
-    process.stdout.write('You typed: ' + cmd);
-    process.stdout.write('\nprompt > ');
-});
-
-pwd();
+    if (cmd === 'pwd') {
+        pwd();
+    } else if (cmd === 'ls') {
+        ls();
+    }
+})
 
 
--- pwd.js
+++ pwd.js
@@ -1,7 +1,5 @@
 module.exports= function() {
-  process.stdin.on('data', (data) => {
-    const pwd = data.toString().trim();
     process.stdout.write(`Current directory: ${process.cwd()}`)
     process.stdout.write('\nprompt > ');
-  })
-}
+  }
+
